bb684080e0e8c2d08c950d990c7641084270bfe7
CamilaApai <Camilaapai@hotmail.com>
2023-07-12 02:30:58 -0300

    CSS for detail view

--- utilities/index.js
+++ utilities/index.js
@@ -50,7 +50,7 @@ Util.buildClassificationGrid = async function(data){
       + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
       grid += '</h2>'
       grid += '<span>$' 
-      + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
+      + '<b>' + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + ' </b>' + '</span>'
       grid += '</div>'
       grid += '</li>'
     })
@@ -69,17 +69,21 @@ Util.buildInventoryDetail = async function(data){
   let detail = "";
   if(data.length > 0){
     data.forEach(vehicle => { 
+      detail += '<div class="detailPage">'
+      detail += '<div class="detailImage">'
       detail +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
       + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
       + 'details"><img src="' + vehicle.inv_image
       +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
       +' on CSE Motors"></a>'
+      detail += '</div>'
       detail += '<div class="detailInfo">'
       detail += '<p>' + '<b>' + vehicle.inv_make + ' ' + vehicle.inv_model + ' Details ' + '</b>' + '</p>'
       detail += '<p>' + '<b>' + 'Price: $' + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</b>' + '</p>'
       detail += '<p>' + '<b>' + 'Description:' + ' </b>' + vehicle.inv_description + '</p>'
       detail += '<p>' + '<b>' + 'Color:' + ' </b>' + vehicle.inv_color + '</p>'
       detail += '<p>' + '<b>' + 'Miles:' + ' </b>' + new Intl.NumberFormat('en-US').format(vehicle.inv_miles) + '</p>'
+      detail += '</div>'
       detail += '</div>'
     })
   } else { 
