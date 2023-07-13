2c25575f82044eead900ecc7b4fcd643c0fec6a1
CamilaApai <113564236+CamilaApai@users.noreply.github.com>
2023-07-13 00:42:51 +0000

    Add Hashed Password to Register View

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -4,9 +4,10 @@
 const accountModel = require("../models/account-model");
 const utilities = require('../utilities')
 // Week 5
-const jwt = require("jsonwebtoken")
-require("dotenv").config()
 const bcrypt = require("bcryptjs")
+//const jwt = require("jsonwebtoken")
+//require("dotenv").config()
+
 
 
 /* ****************************************
@@ -44,13 +45,29 @@ async function buildLogin(req, res, next) {
 async function registerAccount(req, res) {
     let nav = await utilities.getNav()
     const { account_firstname, account_lastname, account_email, account_password } = req.body
+
+    // Hash the password before storing
+    let hashedPassword
+    try {
+      // regular password and cost (salt is generated automatically)
+      hashedPassword = await bcrypt.hashSync(account_password, 10)
+    } catch (error) {
+      req.flash("notice", 'Sorry, there was an error processing the registration.')
+      res.status(500).render("account/register", {
+        title: "Regis tration",
+        nav,
+        errors: null,
+      })
+    }
+
     const regResult = await accountModel.registerAccount(
       account_firstname,
       account_lastname,
       account_email,
-      account_password
-    )
-  
+      hashedPassword
+    ) 
+
+
     if (regResult) {
       req.flash(
         "notice",
